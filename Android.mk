6f8ed645a0a49353ef92c7a25445c7b118b83530
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-07-26 09:02:17 +0200

    Enough c*ckblock

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 # Board specific SELinux policy variable definitions
 ifneq ($(TARGET_EXCLUDE_QCOM_SEPOLICY),true)
-ifeq ($(call is-vendor-board-platform,QCOM),true)
 LOCAL_PATH:= $(call my-dir)
 BOARD_SEPOLICY_DIRS := \
        $(BOARD_SEPOLICY_DIRS) \
@@ -8,6 +7,4 @@ BOARD_SEPOLICY_DIRS := \
        $(LOCAL_PATH)/common \
        $(LOCAL_PATH)/test \
        $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)
-
-endif
 endif
